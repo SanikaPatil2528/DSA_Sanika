2409dfcb47ccffee702592562c34850c300c0779
SanikaPatil2528 <sanikapatil2528@gmail.com>
2025-09-09 09:23:31 +0530

    Time: 0 ms (100%), Space: 22.5 MB (96.53%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        unordered_map<int,int> mpp;
-        vector<int>expected_nums;
-        int j=0;
+        int k=1;
+        int j=1,check=nums[0];
         for (int i=0;i<nums.size();i++)
         {
-            mpp[nums[i]]+=1;
-           if (mpp[nums[i]]==1)
-           {
-            expected_nums.push_back(nums[i]);
+            if (nums[i]==check) continue;
+            k++;
+            check=nums[i];
             nums[j]=nums[i];
             j++;
-           }
         }
-        int k=expected_nums.size();
         return k;
     }
 };
