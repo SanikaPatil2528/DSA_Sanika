ec4b52e405f06f51a9a3712f02126978e1f961b1
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-05-19 12:38:21 +0530

    Time: 0 ms (100%), Space: 10 MB (5.48%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int>st;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{')
+                st.push(s[i]);
+            else{
+                if(s[i]==')'){
+                    if(!st.empty() && st.top()=='(') st.pop();
+                    else return false;
+                }
+                else if(s[i]=='}'){
+                    if(!st.empty() && st.top()=='{') st.pop();
+                    else return false;
+                }
+                else{
+                    if(!st.empty() && st.top()=='[') st.pop();
+                    else return false;
+                }
+            }
+        }
+        if(!st.empty()) return false;
+        return true;
+    }
+};
