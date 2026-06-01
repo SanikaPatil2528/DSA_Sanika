2bc172c1cd9457b6d5fe3bcf283a2e0e789846ea
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-01 15:49:32 +0530

    Time: 1 ms (37.92%), Space: 17.1 MB (39.05%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,48 +3,43 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        //visited array
-        vector<vector<int>> vis(m,vector<int>(n,0));
-        // {{r,c},minutes};
-        queue<pair<pair<int,int>,int>> q;
-        int count1=0;
-        int check=0;
-
+        vector<vector<int>>vis(m,vector<int>(n,0));
+        // {r,c},time
+        queue<pair<pair<int,int>,int>>q;
+        int cnt1=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
-                    q.push({{i,j},0});
                     vis[i][j]=2;
+                    q.push({{i,j},0});
                 }
-                else if(grid[i][j]==1){
-                    count1++;
-                    vis[i][j]=0;
+                else if(grid[i][j]==1) {
+                    cnt1++;
+                    vis[i][j]=1;
                 }
-                else vis[i][j]=0;
             }
         }
-
         int drow[]={0,-1,0,1};
         int dcol[]={-1,0,1,0};
-        int tm=0;
+        int time=0;
+        int check=0;
         while(!q.empty()){
             int r=q.front().first.first;
             int c=q.front().first.second;
             int t=q.front().second;
-            tm=max(tm,t);
             q.pop();
+            time=max(t,time);
             for(int i=0;i<4;i++){
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
-                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                    q.push({{nrow,ncol},t+1});
-                    vis[nrow][ncol]=2;
+                int nr=r+drow[i];
+                int nc=c+dcol[i];
+                if(nr>=0 && nr<m && nc>=0 && nc<n && vis[nr][nc]==1){
+                    vis[nr][nc]=2;
                     check++;
+                    q.push({{nr,nc},t+1});
                 }
             }
         }
-
-        if(count1==check) return tm;
+        if(cnt1==check) return time;
         return -1;
     }
 };
