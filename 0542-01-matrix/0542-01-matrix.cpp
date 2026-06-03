e32648afc061373a4949d387f03deae4c7a3289b
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-03 17:27:09 +0530

    Time: 16 ms (64.91%), Space: 38.6 MB (39.8%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,43 +1,43 @@
 class Solution {
-public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+private:
+    void bfs(vector<vector<int>>&ans,vector<vector<int>>&mat){
         int m=mat.size();
         int n=mat[0].size();
-        vector<vector<int>>ans=mat;
-        bfs(ans,mat,m,n);
-        return ans;
-    }
-
-    void bfs(vector<vector<int>>&ans,vector<vector<int>>&mat,int m,int n){
-        int drow[]={0,-1,0,1};
-        int dcol[]={-1,0,1,0};
-        queue<pair<pair<int,int>,int>>q;
         vector<vector<int>>vis(m,vector<int>(n,0));
+        // {{r,c},dis};
+        queue<pair<pair<int,int>,int>>q;
+        vector<int>drow={-1,0,1,0};
+        vector<int>dcol={0,1,0,-1};
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(ans[i][j]==0)
-                {
-                    q.push({{i,j},0});
+                if(mat[i][j]==0){
                     vis[i][j]=1;
-                } 
+                    q.push({{i,j},0});
+                }
             }
         }
 
         while(!q.empty()){
-            int row=q.front().first.first;
-            int col=q.front().first.second;
-            int distance=q.front().second;
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int d=q.front().second;
             q.pop();
-            ans[row][col]=distance;
-            for(int a=0;a<4;a++){
-                int nrow=row+drow[a];
-                int ncol=col+dcol[a];
-                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]!=1){
-                    q.push({{nrow,ncol},distance+1});
-                    vis[nrow][ncol]=1;
+            for(int i=0;i<4;i++){
+                int nr=r+drow[i];
+                int nc=c+dcol[i];
+                if(nr>=0 && nr<m && nc>=0 && nc<n && !vis[nr][nc] && mat[nr][nc]==1){
+                    vis[nr][nc]=1;
+                    ans[nr][nc]=d+1;
+                    q.push({{nr,nc},d+1});
                 }
             }
         }
+    }
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        vector<vector<int>>ans=mat;
+        bfs(ans,mat);
+        return ans;
     }
 };
