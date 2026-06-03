85080c59e5b0ec59c3b566334c6e80f30d75bc9c
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-03 17:59:25 +0530

    Time: 10 ms (44.4%), Space: 38.9 MB (15.02%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,38 +1,38 @@
 class Solution {
+private:
+    void dfs(int r,int c,vector<vector<int>>&grid,vector<vector<int>>&vis,vector<int>&drow,vector<int>&dcol){
+        vis[r][c]=1;
+        for(int i=0;i<4;i++){
+            int nr=r+drow[i];
+            int nc=c+dcol[i];
+            if(nr>=0 && nr<grid.size() && nc>=0 && nc<grid[0].size() && !vis[nr][nc] && grid[nr][nc]==1){
+                dfs(nr,nc,grid,vis,drow,dcol);
+            }
+        }
+    }
 public:
     int numEnclaves(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
         vector<vector<int>>vis(m,vector<int>(n,0));
-        int drow[]={0,-1,0,1};
-        int dcol[]={-1,0,1,0};
+        vector<int>drow={-1,0,1,0};
+        vector<int>dcol={0,1,0,-1};
 
         for(int i=0;i<m;i++){
-            if(grid[i][0]==1) dfs(i,0,drow,dcol,grid,vis);
-            if(grid[i][n-1]==1) dfs(i,n-1,drow,dcol,grid,vis);
+            if(grid[i][0]==1) dfs(i,0,grid,vis,drow,dcol);
+            if(grid[i][n-1]==1) dfs(i,n-1,grid,vis,drow,dcol);
         }
         for(int i=1;i<n-1;i++){
-            if(grid[0][i]==1) dfs(0,i,drow,dcol,grid,vis);
-            if(grid[m-1][i]==1) dfs(m-1,i,drow,dcol,grid,vis);
+            if(grid[0][i]==1) dfs(0,i,grid,vis,drow,dcol);
+            if(grid[m-1][i]==1) dfs(m-1,i,grid,vis,drow,dcol);
         }
 
-        int g1=0,v1=0;
+        int cnt=0;
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1) g1++;
-                if(vis[i][j]==1) v1++;
-            }
+            for(int j=0;j<n;j++)
+                if(grid[i][j]==1 && vis[i][j]==0) cnt++;
         }
 
-        return g1-v1;
-    }
-
-    void dfs(int row,int col,int drow[],int dcol[],vector<vector<int>>& board,vector<vector<int>>& vis){
-        vis[row][col]=1;
-        for(int i=0;i<4;i++){
-            int nrow=row+drow[i];
-            int ncol=col+dcol[i];
-            if(nrow>=0 && nrow<board.size() && ncol>=0 && ncol<board[0].size() && board[nrow][ncol]==1 && vis[nrow][ncol]==0) dfs(nrow,ncol,drow,dcol,board,vis);
-        }
+        return cnt;
     }
 };
