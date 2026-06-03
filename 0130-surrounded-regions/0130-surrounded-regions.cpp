806e6404a5e09f34229e7a69c1bf5fb312515e61
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-03 17:48:59 +0530

    Time: 0 ms (100%), Space: 14.5 MB (48.57%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,33 +1,36 @@
 class Solution {
+private:
+    void dfs(int r,int c,vector<vector<char>>&board,vector<vector<int>>&vis,vector<int>&drow,vector<int>&dcol){
+        vis[r][c]=1;
+        for(int i=0;i<4;i++){
+            int nr=r+drow[i];
+            int nc=c+dcol[i];
+            if(nr>=0 && nr<board.size() && nc>=0 && nc<board[0].size() && board[nr][nc]=='O' && !vis[nr][nc]){
+                dfs(nr,nc,board,vis,drow,dcol);
+            }
+        }
+    }
 public:
     void solve(vector<vector<char>>& board) {
         int m=board.size();
         int n=board[0].size();
         vector<vector<int>>vis(m,vector<int>(n,0));
-        int drow[]={0,-1,0,1};
-        int dcol[]={-1,0,1,0};
-
+        vector<int>drow={-1,0,1,0};
+        vector<int>dcol={0,1,0,-1};
+        
+        // dfs for boundary O's
         for(int i=0;i<m;i++){
-            if(board[i][0]=='O') dfs(i,0,drow,dcol,board,vis);
-            if(board[i][n-1]=='O') dfs(i,n-1,drow,dcol,board,vis);
+            if(board[i][0]=='O') dfs(i,0,board,vis,drow,dcol);
+            if(board[i][n-1]=='O') dfs(i,n-1,board,vis,drow,dcol);
         }
         for(int i=1;i<n-1;i++){
-            if(board[0][i]=='O') dfs(0,i,drow,dcol,board,vis);
-            if(board[m-1][i]=='O') dfs(m-1,i,drow,dcol,board,vis);
+            if(board[0][i]=='O') dfs(0,i,board,vis,drow,dcol);
+            if(board[m-1][i]=='O') dfs(m-1,i,board,vis,drow,dcol);
         }
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++)
-                if(board[i][j]=='O' && vis[i][j]==0) board[i][j]='X';
-        }
-    }
-
-    void dfs(int row,int col,int drow[],int dcol[],vector<vector<char>>& board,vector<vector<int>>& vis){
-        vis[row][col]=1;
-        for(int i=0;i<4;i++){
-            int nrow=row+drow[i];
-            int ncol=col+dcol[i];
-            if(nrow>=0 && nrow<board.size() && ncol>=0 && ncol<board[0].size() && board[nrow][ncol]=='O' && vis[nrow][ncol]==0) dfs(nrow,ncol,drow,dcol,board,vis);
+                if(vis[i][j]==0) board[i][j]='X';
         }
     }
 };
