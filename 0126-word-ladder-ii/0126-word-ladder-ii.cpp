c80415e3f89d651f8b4d2591b2d3c5fe3cf1609b
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-12 14:50:19 +0530

    Time: 15 ms (45.28%), Space: 13.1 MB (78.23%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.cpp
+++ 0126-word-ladder-ii/0126-word-ladder-ii.cpp
@@ -1,63 +1,59 @@
 class Solution {
-    unordered_map<string,int>mpp;
-    vector<vector<string>>ans;
-    string b;
-
 private:
-    void dfs(string word,vector<string>&seq){
-        if(word==b){
-            reverse(seq.begin(),seq.end());
-            ans.push_back(seq);
-            reverse(seq.begin(),seq.end());
-            return;
+    void dfs(string word,vector<string>&curr,vector<vector<string>>&ans,string beginWord,unordered_map<string,int>&mpp){
+        if(word==beginWord){
+            reverse(curr.begin(),curr.end());
+            ans.push_back(curr);
+            reverse(curr.begin(),curr.end());
+            return ;
         }
-
+        string check=word;
         for(int i=0;i<word.size();i++){
-                char original=word[i];
-                int steps=mpp[word];
-                for(char ch='a'; ch<='z';ch++){
-                    word[i]=ch;
-                    if(mpp.find(word)!=mpp.end() && mpp[word]+1==steps){
-                        seq.push_back(word);
-                        dfs(word,seq);
-                        seq.pop_back();
-                    }
+            char orig=word[i];
+            for(char ch='a';ch<='z';ch++){
+                word[i]=ch;
+                if(mpp.find(word)!=mpp.end() && mpp[word]==mpp[check]-1){
+                    curr.push_back(word);
+                    dfs(word,curr,ans,beginWord,mpp);
+                    curr.pop_back();
                 }
-                word[i]=original;
             }
+            word[i]=orig;
+        }
     }
-
 public:
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
         queue<string>q;
         unordered_set<string>st(wordList.begin(),wordList.end());
+        unordered_map<string,int>mpp;
         q.push(beginWord);
-        mpp[beginWord]=1;
-        st.erase(beginWord);
-        b=beginWord;
+        mpp[beginWord]=0;
+        int minSteps=0;
+
         while(!q.empty()){
             string word=q.front();
             q.pop();
-            int steps=mpp[word];
+            int step=mpp[word];
+            if(word==endWord){
+                minSteps=step;
+                break;
+            }
             for(int i=0;i<word.size();i++){
-                char original=word[i];
+                char orig=word[i];
                 for(char ch='a';ch<='z';ch++){
                     word[i]=ch;
-                    if (st.count(word)){
-                        st.erase(word);
+                    if(st.count(word)>0 && mpp.find(word)==mpp.end()){
+                        mpp[word]=step+1;
                         q.push(word);
-                        mpp[word]=steps+1;
                     }
                 }
-                word[i]=original;
+                word[i]=orig;
             }
         }
 
-        if(mpp.find(endWord)!=mpp.end()){
-            vector<string>seq;
-            seq.push_back(endWord);
-            dfs(endWord,seq);
-        }
+        vector<vector<string>>ans;
+        vector<string>curr={endWord};
+        dfs(endWord,curr,ans,beginWord,mpp);
         return ans;
     }
 };
