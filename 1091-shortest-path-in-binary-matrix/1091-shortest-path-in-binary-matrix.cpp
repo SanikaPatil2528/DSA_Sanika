2a263c50ce0539aa4869b29723c469499af5cb88
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-14 12:05:42 +0530

    Time: 20 ms (45.79%), Space: 26.2 MB (35.04%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,37 +2,34 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
-        int drow[]={0,-1,-1,-1,0,1,1,1};
-        int dcol[]={-1,-1,0,1,1,1,0,-1};
-
-        if(grid[0][0]==1) return -1;
-        if(grid[n-1][n-1]==1) return -1;
-
-        vector<vector<int>>distance(n,vector<int>(n,1e9));
-        distance[0][0]=1;
-        
-        // (dist,{row,col})
-        queue<pair<int,pair<int,int>>> q;
+        if(grid[0][0]==1 || grid[n-1][n-1]==1) return -1;
+        // dis, {r,c}
+        queue<pair<int,pair<int,int>>>q;
         q.push({1,{0,0}});
+        vector<int>drow={-1,-1,0,1,1,1,0,-1};
+        vector<int>dcol={0,1,1,1,0,-1,-1,-1};
+
+        vector<vector<int>>dis(n,vector<int>(n,1e9));
+        dis[0][0]=1;
 
         while(!q.empty()){
-            auto it=q.front();
+            int d=q.front().first;
+            int r=q.front().second.first;
+            int c=q.front().second.second;
             q.pop();
-            int dist=it.first;
-            int row=it.second.first;
-            int col=it.second.second;
+            if(r==n-1 && c==n-1) return d;
 
             for(int i=0;i<8;i++){
-                int newr=row+drow[i];
-                int newc=col+dcol[i];
-                if(newr>=0 && newr<n && newc>=0 && newc<n && grid[newr][newc]==0 && dist+1 < distance[newr][newc]){
-                    distance[newr][newc]=dist+1;
-                    q.push({distance[newr][newc],{newr,newc}});
+                int nr=r+drow[i];
+                int nc=c+dcol[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<n && grid[nr][nc]==0 && d+1<dis[nr][nc]){
+                    dis[nr][nc]=d+1;
+                    q.push({d+1,{nr,nc}});
                 }
             }
         }
 
-        if (distance[n-1][n-1]<1e9) return distance[n-1][n-1];
-        return -1;
+        if(dis[n-1][n-1]==1e9) return -1;
+        return dis[n-1][n-1];
     }
 };
