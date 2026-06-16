092565b5fedbdfbe0dcf158cdb34110a6d7ef713
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-16 10:04:27 +0530

    Time: 8 ms (26.89%), Space: 39.7 MB (69.7%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string res="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]>='a' && s[i]<='z') res=res+s[i];
+            else if(s[i]=='*' && res.size()!=0) res.pop_back();
+            else if(s[i]=='#') res=res+res;
+            else reverse(res.begin(),res.end());
+        }
+        return res;
+    }
+};
