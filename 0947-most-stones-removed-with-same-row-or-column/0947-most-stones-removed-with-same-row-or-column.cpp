99dac334a1e747471cf21907314a852d13bf3d09
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-16 10:57:30 +0530

    Time: 16 ms (67.87%), Space: 24.6 MB (31.63%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,14 +1,14 @@
 class Solution {
 private:
-    int findUPar(int node,vector<int>&parent){
-        if(parent[node]==node) return node;
-        return parent[node]=findUPar(parent[node],parent);
+    int findUPar(int node,vector<int>&par){
+        if(par[node]==node) return node;
+        return par[node]=findUPar(par[node],par);
     }
     void unionBySize(int u,int v,vector<int>&size,vector<int>&par){
-        if(findUPar(u,par)==findUPar(v,par)) return ;
         int up_u=findUPar(u,par);
         int up_v=findUPar(v,par);
-        if(size[up_u]<up_v){
+        if(up_u==up_v) return ;
+        if(size[up_u]<size[up_v]){
             par[up_u]=up_v;
             size[up_v]+=size[up_u];
         }
@@ -19,24 +19,31 @@ private:
     }
 public:
     int removeStones(vector<vector<int>>& stones) {
-        vector<vector<int>>edges;
-        for(int i=0;i<stones.size();i++){
-            for(int j=0;j<stones.size();j++){
-                if(i==j) continue;
-                if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1]) edges.push_back({i,j});
-            }
+        int maxr=0,maxc=0;
+        for(auto stone:stones){
+            maxr=max(maxr,stone[0]);
+            maxc=max(maxc,stone[1]);
         }
-        vector<int>size(stones.size(),1);
-        vector<int>par(stones.size());
-        for(int i=0;i<stones.size();i++)
+        int totNodes=maxr+1+maxc+1;
+        vector<int>size(totNodes,1);
+        vector<int>par(totNodes);
+        for(int i=0;i<totNodes;i++)
             par[i]=i;
         
-        for(auto edge:edges){
-            if(findUPar(edge[0],par)!= findUPar(edge[1],par)) unionBySize(edge[0],edge[1],size,par);
+        unordered_set<int>usedStones;
+        for(auto stone:stones){
+            int r=stone[0];
+            int c=stone[1];
+            int shiftedC=c+maxr+1;
+            unionBySize(r,shiftedC,size,par);
+            usedStones.insert(r);
+            usedStones.insert(shiftedC);
         }
+
         int comp=0;
-        for(int i=0;i<stones.size();i++)
-            if(par[i]==i) comp++;
+        for(auto node:usedStones)
+            if(findUPar(node,par)==node) comp++;
+        
         return stones.size()-comp;
     }
 };
