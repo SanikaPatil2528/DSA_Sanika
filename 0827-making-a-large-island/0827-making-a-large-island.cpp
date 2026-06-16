ffbc52a46b96bbc3ef20f2e39eaceee1a2ec0f60
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-16 16:00:32 +0530

    Time: 287 ms (36.06%), Space: 159.3 MB (40.83%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.cpp
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -1,84 +1,72 @@
-class DisjointSet{
-public:
-    vector<int>size,parent;
-
-    DisjointSet(int n){
-        size.resize(n,1);
-        parent.resize(n);
-        for(int i=0;i<n;i++)
-            parent[i]=i;
-    }
-    int findUParent(int node){
-        if(node==parent[node]) return node;
-        return parent[node]=findUParent(parent[node]);
-    }
-    void UnionBySize(int u,int v){
-        int ult_u=findUParent(u);
-        int ult_v=findUParent(v);
-        if(ult_u==ult_v) return;
-        if(size[ult_u]<size[ult_v]){
-            parent[ult_u]=ult_v;
-            size[ult_v]+=size[ult_u];
-        }
-        else{
-            parent[ult_v]=ult_u;
-            size[ult_u]+=size[ult_v];
-        }
-    }
-};
-
 class Solution {
 private:
-    bool isValid(int row,int col,int n){
-        if (row>=0 && row<n && col>=0 && col<n) return true;
-        return false;
+    int findUPar(int node,vector<int>&par){
+        if(par[node]==node) return node;
+        return par[node]=findUPar(par[node],par);
+    }
+    void unionBySize(int u,int v,vector<int>&size,vector<int>&par){
+        int up_u=findUPar(u,par);
+        int up_v=findUPar(v,par);
+        if(up_u==up_v) return ;
+        if(size[up_u]<size[up_v]){
+            par[up_u]=up_v;
+            size[up_v]+=size[up_u];
+        }
+        else{
+            par[up_v]=up_u;
+            size[up_u]+=size[up_v];
+        }
     }
 public:
     int largestIsland(vector<vector<int>>& grid) {
-        int no_of_1=0;
         int n=grid.size();
-        int drow[]={-1,0,1,0};
-        int dcol[]={0,1,0,-1};
-        DisjointSet ds(n*n);
-
-        // creating components
-        for(int row=0;row<n;row++){
-            for(int col=0;col<n;col++){
-                if (grid[row][col]==0) continue;
-                no_of_1++;
-                for(int ind=0;ind<4;ind++){
-                    int newr=row+drow[ind];
-                    int newc=col+dcol[ind];
-                    if(isValid(newr,newc,n) && grid[newr][newc]==1){
-                        int nodeNo=row*n + col;
-                        int adjNodeNo=newr*n + newc;
-                        ds.UnionBySize(nodeNo,adjNodeNo);
-                    }
-                }
-            }
-        }
-        if (no_of_1==n*n) return n*n;
-
-        // converting each 0 to 1 and checking total size of island
-        int max_size=0;
+        vector<int>size(n*n,1);
+        vector<int>par(n*n);
+        for(int i=0;i<n*n;i++)
+            par[i]=i;
+        vector<int>drow={-1,0,1,0};
+        vector<int>dcol={0,1,0,-1};
+        
+        // connecting 1's and creating islands
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                unordered_set<int>s;
-                if(grid[i][j]==0){
-                    int size_check=0;
-                    for(int ind=0;ind<4;ind++){
-                        int newr=i+drow[ind];
-                        int newc=j+dcol[ind];
-                        if(isValid(newr,newc,n) && grid[newr][newc]==1) s.insert(ds.findUParent(newr*n + newc));
+                if(grid[i][j]==0) continue;
+                int cell=i*n+j;
+                for(int k=0;k<4;k++){
+                    int nr=i+drow[k];
+                    int nc=j+dcol[k];
+                    if(nr>=0 && nr<n && nc>=0 && nc<n && grid[nr][nc]==1){
+                        int newCell=nr*n+nc;
+                        if(findUPar(cell,par)!=findUPar(newCell,par)) unionBySize(cell,newCell,size,par);
                     }
-                    for(auto it: s)
-                        size_check+=ds.size[it];
-                    size_check++; // place of 0 replaced by 1 so incremented the size of component by 1
-                    max_size=max(max_size,size_check);
                 }
             }
         }
 
-        return max_size;
+        // checking for all 0's
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0){
+                    unordered_set<int>st; // to store unique neighbouring components
+                    for(int k=0;k<4;k++){
+                        int nr=i+drow[k];
+                        int nc=j+dcol[k];
+                        if(nr>=0 && nr<n && nc>=0 && nc<n && grid[nr][nc]==1){
+                            st.insert(findUPar(nr*n+nc,par));
+                        }
+                    }
+                    int cnt=1;
+                    for(auto it:st)
+                        cnt+=size[it];
+                    maxi=max(maxi,cnt);
+                }
+            }
+        }
+        if(maxi==0){
+            for(int i=0;i<n*n;i++)
+                maxi=max(maxi,size[i]);
+        }
+        return maxi;
     }
 };
