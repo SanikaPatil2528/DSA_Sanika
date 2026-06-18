6378de1c65a173c977f57b666bd56d595951b0f5
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-18 11:01:53 +0530

    Time: 0 ms (100%), Space: 11.3 MB (7.92%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,22 +1,24 @@
 class Solution {
 private:
-    int helper(vector<int>&nums,int index,vector<int>&dp){
-        if(index==0) return nums[index];
-        if(index<0) return 0;
-        if(dp[index]!=-1) return dp[index];
-        int pick=nums[index]+helper(nums,index-2,dp);
-        int notPick=0+helper(nums,index-1,dp);
-        return dp[index]=max(pick,notPick);
+    int helper(int idx,vector<int>&nums,bool firstPicked,vector<vector<int>>&dp){
+        if(idx==nums.size()-1){
+            if(firstPicked) return 0;
+            return nums[idx];
+        }
+        if(idx>=nums.size()) return 0;
+        if(dp[idx][firstPicked]!=-1) return dp[idx][firstPicked];
+
+        int pick;
+        if(idx==0) pick=nums[idx]+helper(idx+2,nums,true,dp);
+        else pick=nums[idx]+helper(idx+2,nums,firstPicked,dp);
+        int notPick=helper(idx+1,nums,firstPicked,dp);
+
+        return dp[idx][firstPicked]=max(pick,notPick);
     }
 public:
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return nums[0];
-        vector<int>temp(nums.begin()+1,nums.end());
-        vector<int>dp1(n,-1);
-        vector<int>dp2(n-1,-1);
-        int ans1=helper(nums,n-2,dp1); // leave last element
-        int ans2=helper(temp,n-2,dp2); // leave last element
-        return max(ans1,ans2);
+        vector<vector<int>>dp(nums.size(),vector<int>(2,-1));
+        int ans=helper(0,nums,false,dp);
+        return ans;
     }
 };
