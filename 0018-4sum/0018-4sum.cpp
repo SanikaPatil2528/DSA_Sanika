b31f75995a496042b0fb309b5b1a4842279f8d0d
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-19 12:37:38 +0530

    Time: 14 ms (92.56%), Space: 17.4 MB (83.33%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>>ans;
         int n=nums.size();
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+
         for(int i=0;i<n;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
             for(int j=i+1;j<n;j++){
                 if(j>i+1 && nums[j]==nums[j-1]) continue;
-                int k=j+1,l=n-1;
-                while(k<l){
-                    long long sum= (long long)nums[i]+nums[j]+nums[k]+nums[l];
-                    if(sum<target) k++;
-                    else if(sum>target) l--;
-                    else{
-                        vector<int>temp={nums[i],nums[j],nums[k],nums[l]};
-                        ans.push_back(temp);
-                        k++;
-                        l--;
-                        while(k<l && nums[k]==nums[k-1]) k++;
-                        while(k<l && nums[l]==nums[l+1]) l--;
+                int left=j+1,right=n-1;
+                while(left<right){
+                    long long sum=1LL*nums[i]+1LL*nums[j]+1LL*nums[left]+1LL*nums[right];
+                    if(sum==target){
+                        ans.push_back({nums[i],nums[j],nums[left],nums[right]});
+                        left++;
+                        right--;
+                        while(left<right && nums[left]==nums[left-1]) left++;
+                        while(left<right && nums[right]==nums[right+1]) right--;
                     }
+                    else if(sum<target) left++;
+                    else right--;
                 }
             }
         }
