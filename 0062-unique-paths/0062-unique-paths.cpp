69acee38fbbfe82895db33a6e09b5496afeafd1c
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-19 19:44:43 +0530

    Time: 0 ms (100%), Space: 9.3 MB (52.32%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -22,12 +22,29 @@ public:
             for(int c=0;c<n;c++){
                 if(r==0 && c==0) continue;
                 int up=0;
-                if(r>0) up=helper(r-1,c,dp);
+                if(r>0) up=dp[r-1][c];
                 int left=0;
-                if(c>0) left=helper(r,c-1,dp);
+                if(c>0) left=dp[r][c-1];
                 dp[r][c]=up+left;
             }
         }
         return dp[m-1][n-1];
+        
+        // SPACE OPTIMIZATION
+        // vector<int>dp(n,0);
+        // dp[0]=1;
+        // for(int r=0;r<m;r++){
+        //     vector<int>curr(n,0);
+        //     for(int c=0;c<n;c++){
+        //         if(r==0 && c==0) continue;
+        //         int up=0;
+        //         if(r>0) up=dp[r-1][c]dp);
+        //         int left=0;
+        //         if(c>0) left=helper(r,c-1,dp);
+        //         dp[r][c]=up+left;
+        //     }
+        // }
+        // return dp[m-1][n-1];
+        
     }
 };
