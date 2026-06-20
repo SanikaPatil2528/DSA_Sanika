6ae2072d57731aa0b814c49ef4d35c7bc29e3853
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-20 13:54:35 +0530

    Time: 7 ms (0.12%), Space: 12.5 MB (71.45%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,17 +1,23 @@
 class Solution {
-public:
-    int findPeakElement(vector<int>& nums) {
-        if (nums.size()==1) return 0;
+private:    
+    int binarySearch(int low,int high,vector<int>&nums){
         if(nums[0]>nums[1]) return 0;
         if(nums[nums.size()-1]>nums[nums.size()-2]) return nums.size()-1;
-        int low=1,high=nums.size()-2;
+        low=1;
+        high=high-1;
         while(low<=high){
             int mid=low+(high-low)/2;
             if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
-            if(nums[mid]>nums[mid-1]) low=mid+1;
-            else if(nums[mid]>nums[mid+1]) high=mid-1;
+            else if(nums[mid]>nums[mid-1]) low=mid+1;
             else high=mid-1;
         }
         return -1;
+    }
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return 0;
+        int ans=binarySearch(0,n-1,nums);
+        return ans;
     }
 };
