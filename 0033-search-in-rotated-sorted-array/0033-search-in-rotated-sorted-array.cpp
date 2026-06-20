44dd0e03da54b77327435d9dcc9d72da5442142e
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-20 11:02:10 +0530

    Time: 0 ms (100%), Space: 15.1 MB (67.05%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,23 +1,23 @@
 class Solution {
+private:
+    int binarySearch(int low,int high,vector<int>&nums,int target){
+        if(low>high) return -1;
+        int mid=low+(high-low)/2;
+        if(nums[mid]==target) return mid;
+        if(nums[low]<=nums[mid]){
+            if(target>=nums[low] && target<nums[mid]) return binarySearch(low,mid-1,nums,target);
+            else return binarySearch(mid+1,high,nums,target);
+        }
+        else if(nums[mid]<nums[high]){
+            if(target>nums[mid] && target<=nums[high]) return binarySearch(mid+1,high,nums,target);
+            else return binarySearch(low,mid-1,nums,target);
+        } 
+        return -1;
+    }
 public:
     int search(vector<int>& nums, int target) {
-        int low=0,high=nums.size()-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target) return mid;
-            // left sorted
-            if(nums[low]<=nums[mid]){
-                // if ele lies on left
-                if(nums[low]<=target && nums[mid]>=target) high=mid-1;
-                else low=mid+1;
-            }
-            // right sorted
-            else{
-                // if ele on right
-                if(nums[high]>=target && nums[mid]<=target) low=mid+1;
-                else high=mid-1;
-            }
-        }
-        return -1;
+        int n=nums.size();
+        int ans=binarySearch(0,n-1,nums,target);
+        return ans;
     }
 };
