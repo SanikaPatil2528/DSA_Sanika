94bb7135c4dee65938e0af925b28d27bbe57538b
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-23 13:25:55 +0530

    Time: 0 ms (100%), Space: 50.3 MB (58.09%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -6,18 +6,18 @@ public:
         int low=0,high=m-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            int maxEle=INT_MIN,idx=-1;
+            int maxi=-1,maxIdx=-1;
             for(int i=0;i<n;i++){
-                if(mat[mid][i]>maxEle){
-                    maxEle=mat[mid][i];
-                    idx=i;
+                if(mat[mid][i]>maxi){
+                    maxi=mat[mid][i];
+                    maxIdx=i;
                 }
             }
-            int top= mid>0? mat[mid-1][idx]: -1;
-            int bottom= mid<m-1 ?mat[mid+1][idx] :-1;
-            if(mat[mid][idx]>top && mat[mid][idx]>bottom) return {mid,idx};
-            if(mat[mid][idx]>top) low=mid+1;
-            else high=mid-1;
+            int top=mid>0?mat[mid-1][maxIdx]:-1;
+            int bottom=mid<m-1?mat[mid+1][maxIdx]:-1;
+            if(maxi>top && maxi>bottom) return {mid,maxIdx};
+            else if(maxi<top) high=mid-1;
+            else low=mid+1;
         }
         return {-1,-1};
     }
