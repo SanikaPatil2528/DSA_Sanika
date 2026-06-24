5d2e34cf6283f61ea9097a8a4a3a7ff2e331229c
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-24 10:15:23 +0530

    Time: 3 ms (94.18%), Space: 44.9 MB (11.52%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,15 +3,14 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
-        int l=0,r=0;
-        int n=g.size(),m=s.size();
-        while(l<n && r<m){
-            if(s[r]<g[l]) r++;
-            else{
-                l++;
-                r++;
+        int g1=0,s1=0;
+        while(g1<g.size() && s1<s.size()){
+            if(s[s1]>=g[g1]){
+                g1++;
+                s1++;
             }
+            else s1++;
         }
-        return l;
+        return g1;
     }
 };
