764b93757bcade99ee9ad99eb01277460d5c8438
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-25 11:33:54 +0530

    Time: 679 ms (5%), Space: 378.9 MB (5.01%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,22 +1,21 @@
 class Solution {
 private:
-    int lcs(string &s1,string &s2,int idx1,int idx2,vector<vector<int>>&dp){
-        if(idx1<0 || idx2<0) return 0;
-        if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
-
-        // match
-        if(s1[idx1]==s2[idx2])
-            return dp[idx1][idx2]= 1 + lcs(s1,s2,idx1-1,idx2-1,dp);
-        // not match
-        return dp[idx1][idx2]= 0 + max(lcs(s1,s2,idx1-1,idx2,dp), lcs(s1,s2,idx1,idx2-1,dp));
+    int helper(int i,int j,string s1,string s2,vector<vector<int>>&dp){
+        if(i>=s1.size() || j>=s2.size()) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1[i]==s2[j]) return dp[i][j]=1+helper(i+1,j+1,s1,s2,dp);
+        else return dp[i][j]=max(helper(i+1,j,s1,s2,dp),helper(i,j+1,s1,s2,dp));
+    }
+    int lcs(string str1, string str2) {
+        vector<vector<int>>dp(str1.size(),vector<int>(str2.size(),-1));
+        int ans=helper(0,0,str1,str2,dp);
+        return ans;
     }
 public:
     int minDistance(string word1, string word2) {
-        vector<vector<int>>dp(word1.size(),vector<int>(word2.size(),-1));
-        int ans=lcs(word1,word2,word1.size()-1,word2.size()-1,dp);
-        int count1,count2;
-        count1=word1.size()-ans;
-        count2=word2.size()-ans;
-        return count1+count2;
+        int l=lcs(word1,word2);
+        int rem1=word1.size()-l;
+        int rem2=word2.size()-l;
+        return rem1+rem2;
     }
 };
