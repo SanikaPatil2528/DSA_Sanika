127603da92d7092bbe8aae38d32b122f9ea19920
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-25 22:14:30 +0530

    Time: 0 ms (100%), Space: 9.6 MB (92.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size()) return false;
-        int n=s.size();
         vector<int>hash(26,0);
-        for(int i=0;i<n;i++) 
-            hash[s[i]-'a']++;
-        for(int i=0;i<n;i++)
-            hash[t[i]-'a']--;
-        for(int i=0;i<26;i++){
-            if (hash[i]!=0) return false;
-        }
+        for(char ch:s) hash[ch-'a']++;
+        for(char ch:t) hash[ch-'a']--;
+        for(int num:hash)
+            if(num) return false;
         return true;
     }
 };
