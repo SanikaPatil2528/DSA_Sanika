bae067bcad853c1bb84660263a8c4c20f4f052cc
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-25 21:54:59 +0530

    Time: 4 ms (23.85%), Space: 17.9 MB (39.22%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        int n=num.size();
-        if ((num[n-1]-'0')%2==1) return num;
-        for(int i=n-1;i>=0;i--){
-            if((num[i]-'0')%2==1){
-                string ans=num.substr(0,i+1);
-                return ans;
-            }
-        }
-        return "";
+        int i=num.size()-1;
+        while(i>=0 && (num[i]-'0')%2==0) i--;
+        return num.substr(0,i+1);
     }
 };
