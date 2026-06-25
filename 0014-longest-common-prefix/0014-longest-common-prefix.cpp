c42a7da9411f6f254291e9b6727bb47de349f55a
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-25 22:07:32 +0530

    Time: 2 ms (11.07%), Space: 12.1 MB (14.47%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        sort(strs.begin(),strs.end());
-        int n=strs.size();
-        int i=0;
-        string first=strs[0],last=strs[n-1];
-        int n1=first.size(),n2=last.size();
-        while(i<min(n1,n2)){
-            if(first[i]!=last[i]) break;
-            i++;
+        // M1-sort then take lcp of first and last word
+        // M2-for each adjacent pair of words take commmon and take common of it with next word
+        string common=strs[0];
+        for(int i=1;i<strs.size();i++){
+            string s1=strs[i];
+            int idx1=0,idx2=0;
+            string temp="";
+            while(idx1<common.size() && idx2<s1.size() && common[idx1]==s1[idx2]){
+                temp+=common[idx1];
+                idx1++;
+                idx2++;
+            }
+            common=temp;
         }
-        string ans=first.substr(0,i);
-        return ans;
+        return common;
     }
 };
