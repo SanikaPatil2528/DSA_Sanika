933604af382db2291aac43de20b41ea8c692c93c
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-25 17:30:02 +0530

    Time: 142 ms (9.33%), Space: 54.8 MB (8.72%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,25 +1,24 @@
 class Solution {
 private:
-    bool helper(string &s1,string &s2,int i,int j,vector<vector<int>>&dp){
-        if (i<0 && j<0) return true;
-        if (j<0 && i>=0) return false;
-        if(i<0){
-            for(int k=0;k<=j;k++){
-                if (s2[k]!='*') return false;
-            }
-            return true;
+    bool helper(int i,int j,string &s,string &p,vector<vector<int>>&dp){
+        if(i==s.size() && j==p.size()) return dp[i][j]=true;
+        if(j==p.size() && i<s.size()) return dp[i][j]=false;
+        if(i==s.size() && j<p.size()){
+            for(int k=j;k<p.size();k++)
+                if(p[k]!='*') return dp[i][j]=false;
+            return dp[i][j]=true;
         }
-        if (dp[i][j]!=-1) return dp[i][j];
-        //match
-        if(s1[i]==s2[j] || s2[j]=='?') return dp[i][j]=helper(s1,s2,i-1,j-1,dp);
-        //not match
-        else if (s2[j]=='*') return dp[i][j]=(helper(s1,s2,i,j-1,dp) || helper(s1,s2,i-1,j,dp));
-        else return false;
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(s[i]==p[j]) return dp[i][j]=helper(i+1,j+1,s,p,dp);
+        if(p[j]=='?') return dp[i][j]=helper(i+1,j+1,s,p,dp);
+        if(p[j]=='*') return dp[i][j]=(helper(i,j+1,s,p,dp) || helper(i+1,j,s,p,dp));
+        return false;
     }
 public:
     bool isMatch(string s, string p) {
-        vector<vector<int>>dp(s.size(),vector<int>(p.size(),-1));
-        bool ans=helper(s,p,s.size()-1,p.size()-1,dp);
+        vector<vector<int>>dp(s.size()+1,vector<int>(p.size()+1,-1));
+        bool ans=helper(0,0,s,p,dp);
         return ans;
     }
 };
