b7a282e4332bd2b02b58591140185577881d205d
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-25 19:31:10 +0530

    Time: 23 ms (87.86%), Space: 26.8 MB (70.4%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,46 +1,40 @@
 class Solution {
-private: 
-    string lcs(string &s1,string &s2){
-        vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size()+1,0));
-        for(int i=1;i<=s1.size();i++){
-            for(int j=1;j<=s2.size();j++){
-                //match
-                if(s1[i-1]==s2[j-1]) dp[i][j]=1+dp[i-1][j-1];
-                // not match
-                else dp[i][j]=0+ max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-        string s;
-        int i=s1.size(), j=s2.size();
-        while(i>0 && j>0){
-            if (s1[i-1]==s2[j-1]){
-                s+=s1[i-1];
-                i--;j--;
-            }
-            else if(dp[i-1][j]>dp[i][j-1]){
-                s+=s1[i-1];
-                i--;
-            } 
-            else{
-                s+=s2[j-1];
-                j--;
-            } 
-        }
-        while(j>0){
-            s+=s2[j-1];
-            j--;
-        }
-        while(i>0){
-            s+=s1[i-1];
-            i--;
-        }
-        reverse(s.begin(),s.end());
-        return s;
-    }
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        string s=str1+str2;
-        string ans=lcs(str1,str2);
+        vector<vector<int>>dp(str1.size()+1,vector<int>(str2.size()+1,0));
+        for(int i=str1.size()-1;i>=0;i--){
+            for(int j=str2.size()-1;j>=0;j--){
+                if(str1[i]==str2[j]) dp[i][j]=1+dp[i+1][j+1];
+                else dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+            }
+        }
+        string ans="";
+        int i=0,j=0;
+        while(i<str1.size() && j<str2.size()){
+            if(str1[i]==str2[j]) {
+                ans+=str1[i];
+                i++;
+                j++;
+            }
+            else{
+                if(dp[i+1][j]>dp[i][j+1]){
+                    ans+=str1[i];
+                    i++;
+                }
+                else{
+                    ans+=str2[j];
+                    j++;
+                }
+            }
+        }
+        while(i>=str1.size() && j<str2.size()) {
+            ans+=str2[j];
+            j++;
+        }
+        while(j>=str2.size() && i<str1.size()) {
+            ans+=str1[i];
+            i++;
+        }
         return ans;
     }
 };
