3254d194ad761a7570394871d704719edbe9f2cc
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-25 16:50:36 +0530

    Time: 63 ms (9.49%), Space: 83.4 MB (5.84%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,21 +1,20 @@
 class Solution {
 private:
-    int helper(string &s1,string &s2,int i,int j,vector<vector<int>>&dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        // match
-        if(s1[i]==s2[j]) return dp[i][j]=0+helper(s1,s2,i-1,j-1,dp);
-        //not match
-        int insert=1+helper(s1,s2,i,j-1,dp);
-        int deletion=1+helper(s1,s2,i-1,j,dp);
-        int replace=1+helper(s1,s2,i-1,j-1,dp);
-        return dp[i][j]=min(insert,min(deletion,replace));
+    int helper(int idx1,int idx2,string word1,string word2,vector<vector<int>>&dp){
+        if(idx1==word1.size()) return word2.size()-idx2;
+        if(idx2==word2.size()) return word1.size()-idx1;
+        if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
+
+        if(word1[idx1]==word2[idx2]) return dp[idx1][idx2]=helper(idx1+1,idx2+1,word1,word2,dp);
+        int deletion=1+helper(idx1+1,idx2,word1,word2,dp);
+        int insertion=1+helper(idx1,idx2+1,word1,word2,dp);
+        int replace=1+helper(idx1+1,idx2+1,word1,word2,dp);
+        return dp[idx1][idx2]=min({deletion,insertion,replace});
     }
 public:
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size(),vector<int>(word2.size(),-1));
-        int ans=helper(word1,word2,word1.size()-1,word2.size()-1,dp);
+        vector<vector<int>>dp(word1.size(),vector<int>(word2.size(),-1));
+        int ans=helper(0,0,word1,word2,dp);
         return ans;
     }
 };
