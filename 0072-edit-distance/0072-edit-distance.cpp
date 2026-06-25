a03b6872fbe3f557cda43f2bf90f5a77f4604c25
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-25 16:57:49 +0530

    Time: 3 ms (91.33%), Space: 13.3 MB (40.5%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -13,8 +13,27 @@ private:
     }
 public:
     int minDistance(string word1, string word2) {
-        vector<vector<int>>dp(word1.size(),vector<int>(word2.size(),-1));
-        int ans=helper(0,0,word1,word2,dp);
-        return ans;
+        // vector<vector<int>>dp(word1.size(),vector<int>(word2.size(),-1));
+        // int ans=helper(0,0,word1,word2,dp);
+        // return ans;
+
+        // TABULATION
+        vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1,0));
+        for(int i=0;i<=word2.size();i++)
+            dp[word1.size()][i]=word2.size()-i;
+        for(int i=0;i<=word1.size();i++)
+            dp[i][word2.size()]=word1.size()-i;
+        for(int idx1=word1.size()-1;idx1>=0;idx1--){
+            for(int idx2=word2.size()-1;idx2>=0;idx2--){
+                if(word1[idx1]==word2[idx2]) dp[idx1][idx2]=dp[idx1+1][idx2+1];
+                else{
+                    int deletion=1+dp[idx1+1][idx2];
+                    int insertion=1+dp[idx1][idx2+1];
+                    int replace=1+dp[idx1+1][idx2+1];
+                    dp[idx1][idx2]=min({deletion,insertion,replace});
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
