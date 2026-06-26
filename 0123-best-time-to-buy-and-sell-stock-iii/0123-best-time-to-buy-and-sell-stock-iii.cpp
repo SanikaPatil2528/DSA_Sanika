4792be62d3dac2b32d573619f940efd17002aa87
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-26 11:00:51 +0530

    Time: 457 ms (20.07%), Space: 225.5 MB (12.14%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,25 +1,26 @@
 class Solution {
 private:
-    int helper(vector<int>&prices,int idx,int buy,int cap,vector<vector<vector<int>>>&dp){
-        if (cap==0) return 0;
-        if (idx==prices.size()) return 0;
-        // allowed to buy
-        // buy, not buy
-        if(buy) {
-            if (dp[idx][1][cap]!=-1) return dp[idx][1][cap];
-            else return dp[idx][1][cap]=max(-prices[idx]+helper(prices,idx+1,0,cap,dp) , 0+helper(prices,idx+1,1,cap,dp) );
+    int helper(int idx,int canBuy,vector<int>&prices,vector<vector<vector<int>>>&dp,int k){
+        if(idx==prices.size()) return 0;
+        if(k==0) return 0;
+        if(dp[idx][canBuy][k]!=-1) return dp[idx][canBuy][k];
+        if(canBuy){
+            // buy,not buy.... +sell -buy
+            int buy=-1*prices[idx]+helper(idx+1,!canBuy,prices,dp,k);
+            int notBuy=helper(idx+1,canBuy,prices,dp,k);
+            return dp[idx][canBuy][k]=max(buy,notBuy);
         }
-        // not allowed
-        // sell, not sell
         else{
-            if (dp[idx][0][cap]!=-1) return dp[idx][0][cap];
-            else return dp[idx][0][cap]=max(prices[idx]+helper(prices,idx+1,1,cap-1,dp) , 0+helper(prices,idx+1,0,cap,dp) );
+            // sell,not sell
+            int sell=prices[idx]+helper(idx+1,!canBuy,prices,dp,k-1);
+            int notSell=helper(idx+1,canBuy,prices,dp,k);
+            return dp[idx][canBuy][k]=max(sell,notSell);
         }
     }
 public:
     int maxProfit(vector<int>& prices) {
         vector<vector<vector<int>>>dp(prices.size(),vector<vector<int>>(2,vector<int>(3,-1)));
-        int ans=helper(prices,0,1,2,dp);
+        int ans=helper(0,1,prices,dp,2);
         return ans;
     }
 };
