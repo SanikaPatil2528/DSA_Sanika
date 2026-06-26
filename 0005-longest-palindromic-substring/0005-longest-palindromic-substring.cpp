f43b39b898e59ca2b7fd9ce7883bb1a0043fc810
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-06-26 16:50:28 +0530

    Time: 7 ms (86.71%), Space: 9.4 MB (79.99%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
+        int paliStart=-1,paliEnd=-1;
+        int maxi=0;
         int n=s.size();
-        int maxLen=1;
-        int start=0;
         // odd lengths
-        for(int i=1;i<n-1;i++){
-            int len=1;
-            int left=i-1,right=i+1;
-            while(left>=0 && right<n && s[left]==s[right]){
-                len+=2;
-                if(len>maxLen){
-                    maxLen=len;
-                    start=left;
-                }
-                left--;right++;
+        for(int i=0;i<n;i++){
+            int left=i;
+            int right=i;
+            while(left-1>=0 && right+1<n && s[left-1]==s[right+1]){
+                left--;
+                right++;
+            }
+            int len=right-left+1;
+            if(len>maxi){
+                maxi=len;
+                paliStart=left;
+                paliEnd=right;
             }
         }
         // even lengths
         for(int i=0;i<n-1;i++){
-            if(s[i]==s[i+1]){
-                int len=2;
-                int left=i-1,right=i+2;
-                    while(left>=0 && right<n && s[left]==s[right]){
-                        len+=2;
-                        left--;right++;
-                    }
-                    if(len>maxLen){
-                        maxLen=len;
-                        start=left+1;
-                    }
+            int left=i;
+            int right=i+1;
+            if(s[left]!=s[right]) continue;
+            while(left-1>=0 && right+1<n && s[left-1]==s[right+1]){
+                left--;
+                right++;
+            }
+            int len=right-left+1;
+            if(len>maxi){
+                maxi=len;
+                paliStart=left;
+                paliEnd=right;
             }
         }
-
-        return s.substr(start,maxLen);
+        if(paliStart==-1 && paliEnd==-1) return "";
+        return s.substr(paliStart,paliEnd-paliStart+1);
     }
 };
