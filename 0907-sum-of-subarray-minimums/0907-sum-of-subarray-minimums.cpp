ebb16fcd8c9e7f5dcea3f29ae97ffcb796eb6c6f
SanikaPatil2528 <sanikapatil2528@gmail.com>
2026-08-03 12:53:15 +0530

    Time: 64 ms (72.05%), Space: 193.6 MB (18.05%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,42 +1,40 @@
 class Solution {
-const int mod=1e9+7;
 private:
-    vector<int>findNSE(vector<int>&arr){
-        int n=arr.size();
-        vector<int>nse(n);
+    vector<int>NSE(vector<int>&arr){
+        vector<int>nse(arr.size());
         stack<int>st;
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && arr[i]<=arr[st.top()])
-                st.pop();
-            nse[i]=st.empty()?n:st.top();
-            st.push(i); 
+        for(int i=arr.size()-1;i>=0;i--){
+            while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
+            if(st.empty()) nse[i]=arr.size();
+            else nse[i]=st.top();
+            st.push(i);
         }
         return nse;
     }
-    vector<int>findPSE(vector<int>&arr){
-        int n=arr.size();
-        vector<int>pse(n);
+    vector<int>PSE(vector<int>&arr){
+        vector<int>pse(arr.size());
         stack<int>st;
-        for(int i=0;i<n;i++){
-            while(!st.empty() && arr[i]<arr[st.top()])
-                st.pop();
-            pse[i]=st.empty()?-1:st.top();
+        for(int i=0;i<arr.size();i++){
+            while(!st.empty() && arr[st.top()]>arr[i]) st.pop();
+            if(st.empty()) pse[i]=-1;
+            else pse[i]=st.top();
             st.push(i);
         }
         return pse;
     }
 public:
+    const int MOD=1e9+7;
+
     int sumSubarrayMins(vector<int>& arr) {
-        int ans=0;
         int n=arr.size();
-        vector<int>nse=findNSE(arr);
-        vector<int>pse=findPSE(arr);
+        vector<int>nse=NSE(arr);
+        vector<int>pse=PSE(arr);
+        long long ans=0;
         for(int i=0;i<n;i++){
             int left=i-pse[i];
             int right=nse[i]-i;
-            long long occurences=(long long)left*right;
-            ans=(ans+arr[i]*occurences)%mod;
+            ans+=(1LL*left*right*arr[i]);
         }
-        return ans;
+        return ans % MOD;
     }
 };
